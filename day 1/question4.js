87aaab62e592c6d2818f5b8574bf3ae4ca896b1b
RAJKUMAR SAINI <rajsaini.com27@gmail.com>
2023-10-22 22:53:31 +0530

    fast commit

---
+++ day 1/question4.js
@@ -0,0 +1,23 @@
+/* PROBLEM 4
+Problem 4
+Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+Print the report card of the student (You can make it look nice by using some keyboard symbols )
+Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration*/
+
+let name = "raj"
+let school = "mittal"
+let grade = "A+" 
+let section = "cap06" 
+let rollno = 34;
+let hindi = 70;
+let eng = 90;
+let computer = 95;
+
+console.log(name)
+console.log(school)
+console.log(grade)
+  console.log(section)
+    console.log(rollno)
+      console.log(hindi)
+console.log(eng)
+console.log(computer)
---
+++ day 2/q1.js
@@ -0,0 +1,2 @@
+let name = "raj";
+console.log(name);
